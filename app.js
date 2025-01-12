66baba4e7ef2c9a02c2f1c590067103f1e5d4585
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-01-12 23:27:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import React from "https://esm.sh/react@19/?dev"
 import ReactDOMClient from "https://esm.sh/react-dom@19/client?dev"
 
-var Header = <div id='Header'>... Header ...</div>;
-var MainArea = <div id='MainArea'>... MainArea ...</div>;
+var Header = () => <div id='Header'>... Header ...</div>;
+var MainArea = () => <div id='MainArea'>... MainArea ...</div>;
 
 // RENDER IT
-var rootElement = <div id='root'>{[ Header, MainArea ]}</div>;
+var rootElement = <div id='root'><Header/><MainArea/></div>;
 ReactDOMClient.createRoot( 
   document.getElementById('app') 
 ).render(rootElement);
